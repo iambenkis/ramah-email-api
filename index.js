78487096163d19247543f7a29aea3bc18a09f35e
benjamin <benkisenge03@gmail.com>
2024-05-21 22:35:27 +0200

    Create Server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+const newCourse = require("./routes/send-email");
+
+app.use("/api", newCourse);
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
